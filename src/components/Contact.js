62bd8ed7f6355dead7958af15265f055be2540c1
LukaszPaciorek <lukasz.paciorek91@gmail.com>
2020-01-05 17:31:54 +0100

    'contactInputs'

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,25 +12,30 @@ function Contact() {
         </div>
         <div className='contact__decoration'>
         </div>
-        <div className='contact__forms'>
-          <div className='forms__input'>
-          <label>Wpisz swoje imię</label>
-          <input type="text" id="text__input" className="name__input"></input>
-          <p className="error__msg">Podane imię jest nieprawidłowe!</p>
-          </div>
-          <div className='forms__input'>
-          <label>Wpisz swój email</label>
-          <input type="email" id="email__input" className="email__input"></input>
-          <p className="error__msg">Podany adres email jest nieprawidłowy!!</p>
+        <div className='contact__forms__container'>
+          <div className='contact__forms'>
+            <div className='forms__input'>
+            <label>Wpisz swoje imię</label>
+            <input type="text" id="text__input" className="name__input" placeholder='Imie' required></input>
+            <div className="error__msg">Podane imię jest nieprawidłowe!</div>
+            </div>
+            <div className='forms__input'>
+            <label>Wpisz swój email</label>
+            <input type="email" id="email__input" className="email__input" placeholder='E-mail' required></input>
+            <div className="error__msg">Podany adres email jest nieprawidłowy!!</div>
+            </div>
           </div>
         </div>
-        <div className='forms__input__text'>
-        <label>Wpisz swój tekst</label>
-        <input type="text__area" id="text__area__input" className="text__area__input"></input>
-        <p className="error__msg">Wiadomość musi mieć conajmniej 120 znaków!</p>
-        </div>
-        <div className='forms__button'>
-          <button type='submit' className='forms__button__submit'>Wyślij</button>
+
+        <div className='contact__text__container'>
+          <div className='forms__input__text'>
+            <label>Wpisz swój tekst</label>
+            <input type="text__area" id="text__area__input" className="text__area__input" rows="5" placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie dolor ac tincidunt blandit. Sed pulvinar lorem id convallis accumsan. '></input>
+            <div className="error__msg">Wiadomość musi mieć conajmniej 120 znaków!</div>
+            <div className='forms__button'>
+              <button type='submit' className='forms__button__submit'>Wyślij</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
