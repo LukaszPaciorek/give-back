73ae529ba260742c57cafd96bfc3fc4651f50802
LukaszPaciorek <lukasz.paciorek91@gmail.com>
2020-01-12 16:35:14 +0100

    'backlog_should_have_done'

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 
-class Contact1 extends React.Component {
+class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -9,14 +9,28 @@ class Contact1 extends React.Component {
       fields: {
         name: '',
         email: '',
-        text: '',
+        message: '',
       },
       errors: {},
-      formIsValid: ''
+      formIsValid: '',
+      status: false
     };
   }
 
-  handleValidation() {
+  sendData = () => {
+
+    let {name, email, message} = this.state.fields;
+
+    fetch('https://fer-api.coderslab.pl/v1/portfolio/contact', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({name: name, email: email, message: message})
+    }).then((res) => res.json())
+        .then((data) => console.log("status", data))
+        .catch((err) => console.log(err))
+};
+
+  handleValidation = () => {
     let fields = this.state.fields;
     let errors = {};
     let formIsValid = true;
@@ -41,10 +55,10 @@ class Contact1 extends React.Component {
       errors['email'] = 'Email nie jest prawidłowy';
     }
 
-    //Text
-    if(fields['text'].length < 120 ) {
+    //text
+    if(fields['message'].length < 120 ) {
       formIsValid = false;
-      errors['text'] = 'Pole powinno zawierać wiadomość z minimum 120 znaków';
+      errors['message'] = 'Pole powinno zawierać wiadomość z minimum 120 znaków';
     }
 
      this.setState({errors: errors});
@@ -58,21 +72,37 @@ class Contact1 extends React.Component {
      console.log(`Błąd: ${errors.email}`);
      console.log(`Czy walidacja email jest nieprawidłowa? ${emailValid}`)
 
-     let textValid = (errors.text !== undefined );
-     console.log(`Błąd: ${errors.text}`);
-     console.log(`Czy walidacja text jest nieprawidłowa? ${textValid}`)
+     let messageValid = (errors.message !== undefined );
+     console.log(`Błąd: ${errors.message}`);
+     console.log(`Czy walidacja message jest nieprawidłowa? ${messageValid}`)
 
      console.log(`Czy walidacja przebiegła pomyślnie? ${formIsValid}`)
+
+
+     if (errors.name === undefined && errors.email === undefined && errors.message === undefined) {
+                 this.setState({
+                     status: true,
+                     name: "",
+                     email: "",
+                     message: ""
+                 });
+                 this.sendData();
+
+             } else {
+                 this.setState({
+                     status: false
+                 })
+             }
 
      return this.setState({formIsValid});
    }
 
-   contactSubmit(e) {
+   contactSubmit = (e) => {
      e.preventDefault();
      this.handleValidation()
    }
 
-   handleChange(field, e) {
+   handleChange = (field, e) => {
      let fields = this.state.fields;
      let formIsValid = this.state.formIsValid;
      fields[field] = e.target.value;
@@ -80,11 +110,22 @@ class Contact1 extends React.Component {
      this.setState({formIsValid});
    }
 
+
+
+
+
+
+
+
+
+
+
+
   render() {
     let formIsValid = (this.state.formIsValid);
     let nameValid = (this.state.errors.name === undefined );
     let emailValid = (this.state.errors.email === undefined );
-    let textValid = (this.state.errors.text === undefined );
+    let messageValid = (this.state.errors.message === undefined );
 
     return (
       <div className='contact' id='contact'>
@@ -106,7 +147,8 @@ class Contact1 extends React.Component {
               <input refs='name' type='text' id='text__input' className='name__input'
                      placeholder='Imie'
                      onChange={this.handleChange.bind(this, 'name')}
-                     value={this.state.fields['name']}></input>
+                     value={this.state.fields['name']}
+                     style={nameValid ? {borderBottom: '1px solid $color__grey'} : {borderBottom: '1px solid red'}}></input>
               <p className='error__name' style={nameValid ? {opacity: '0'} : {opacity: '1'}}>Podane imię jest nieprawidłowe!</p>
               </div>
               <div className='forms__input'>
@@ -114,18 +156,20 @@ class Contact1 extends React.Component {
               <input refs='email' type='email' id='email__input' className='email__input'
                     placeholder='E-mail'
                     onChange={this.handleChange.bind(this, 'email')}
-                    value={this.state.fields['email']} ></input>
+                    value={this.state.fields['email']}
+                    style={emailValid ? {borderBottom: '1px solid $color__grey'} : {borderBottom: '1px solid red'}}></input>
               <p className='error__email' style={emailValid ? {opacity: '0'} : {opacity: '1'}}>Podany adres email jest nieprawidłowy!!</p>
               </div>
               <div className='contact__text__container'>
                 <div className='forms__input__text'>
                     <label>Wpisz swoją wiadomość</label>
-                    <textarea onfocus="if(this.value==this.defaultValue)this.value='';"
+                    <textarea onFocus="if(this.value==this.defaultValue)this.value='';"
                               className='forms__textarea' wrap='hard' cols={40} rows={10}
                               placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean molestie dolor ac tincidunt blandit. Sed pulvinar lorem id convallis accumsan.'
-                              onChange={this.handleChange.bind(this, 'text')}
-                              value={this.state.fields['text']}/>
-                  <p className="error__txt" style={textValid? {opacity: '0'} : {opacity: '1'}}  >Wiadomość musi mieć conajmniej 120 znaków!</p>
+                              onChange={this.handleChange.bind(this, 'message')}
+                              value={this.state.fields['message']}
+                              style={messageValid ? {borderBottom: '1px solid $color__grey'} : {borderBottom: '1px solid red'}}/>
+                  <p className="error__txt" style={messageValid? {opacity: '0'} : {opacity: '1'}}  >Wiadomość musi mieć conajmniej 120 znaków!</p>
                 </div>
               </div>
               <div className='forms__button'>
@@ -148,4 +192,4 @@ class Contact1 extends React.Component {
   }
 }
 
-export { Contact1 };
+export { Contact };
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,9 +4,96 @@ import { HomeHeaderLink } from './HomeHeaderLink';
 import { NavLink } from 'react-router-dom';
 import { Link, animateScroll as scroll } from "react-scroll";
 
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: {
+        email: '',
+        password: '',
+        confirmPassword: ''
+      },
+      formIsValid: '',
+      errors: {}
+    }
+  }
+
+  handleValidation = () => {
+    let fields = this.state.fields;
+    let errors = {};
+    let formIsValid = true;
+
+    //Email
+    let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(String(fields['email']).toLowerCase()) !== true) {
+      formIsValid = false;
+      errors['email'] = 'Email nie jest prawidłowy';
+    }
+
+    //Password
+    if(fields['password'].length < 6 ) {
+      formIsValid = false;
+      errors['password'] = 'Hasło powinno zawierać minimum 6 znaków';
+    }
+
+    //ConfirmPassword
+    if(fields['password'] !== fields['confirmPassword']) {
+      formIsValid = false
+      errors['confirmPassword'] = 'Hasła nie są identyczne';
+    }
+
+    this.setState({errors: errors});
+
+    let emailValid = (errors.email !== undefined );
+    console.log(`Błąd: ${errors.email}`);
+    console.log(`Czy walidacja email jest nieprawidłowa? ${emailValid}`)
+
+    let passwordValid = (errors.password !== undefined );
+    console.log(`Błąd: ${errors.password}`);
+    console.log(fields['password'].length)
+    console.log(`Czy walidacja password jest nieprawidłowa? ${passwordValid}`)
+
+    let confirmPasswordValid = (errors.confirmPassword !== undefined );
+    console.log(`Błąd: ${errors.confirmPassword}`);
+    console.log(`Czy walidacja confirmPassword jest nieprawidłowa? ${confirmPasswordValid}`)
+
+    if (errors.email === undefined && errors.password === undefined && errors.confirmPassword === undefined) {
+                this.setState({
+                    formIsValid: true,
+                    fields: {
+                      name: "",
+                      email: "",
+                      message: ""
+                    }
+                });
+            } else {
+                this.setState({
+                    status: false
+                })
+            }
+    console.log(this.state.fields)
+   }
+
+  handleClick = (e) => {
+    e.preventDefault();
+    this.handleValidation();
+  }
+
+  handleChange = (field, e) => {
+    let fields = this.state.fields;
+    let formIsValid = this.state.formIsValid;
+    fields[field] = e.target.value;
+    this.setState({fields});
+    this.setState({formIsValid});
+  }
+
+  render() {
+
+    let emailValid = (this.state.errors.email !== undefined );
+    let passwordValid = (this.state.errors.password !== undefined );
+    let confirmPasswordValid = (this.state.errors.confirmPassword !== undefined );
 
 
-const Register = () => {
     return  (
 
         <div className='account'>
@@ -21,23 +108,27 @@ const Register = () => {
             <div className='account__login__inputs__reg'>
               <div className='account__email'>
                 <h3>Email</h3>
-                <input type='email'></input>
+                <input type='email'
+                  onChange={this.handleChange.bind(this, 'email')}></input>
               </div>
               <div className='account__password'>
                 <h3>Hasło</h3>
-                <input type='password'></input>
+                <input type='password'
+                  onChange={this.handleChange.bind(this, 'password')}></input>
                 <h3>Powtórz hasło</h3>
-                <input type='password'></input>
+                <input type='password'
+                  onChange={this.handleChange.bind(this, 'confirmPassword')}></input>
               </div>
             </div>
           </div>
           <div className='account__buttons'>
             <NavLink to='logowanie'><button>Zaloguj się</button></NavLink>
-            <button>Załóż konto</button>
+            <button onClick={this.handleClick}>Załóż konto</button>
           </div>
         </div>
         </div>
     )
+  }
 }
 
 export { Register }
